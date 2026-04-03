19e379a63afa9f450fe630a64cba5ed75a79e186
Shailendra2011 <shailendra.c@shyenatechyarns.in>
2026-04-03 07:15:41 +0530

    remove code write from scratch

--- app/core/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-class Settings(BaseSettings):
-    GROQ_API_KEY: str
-    CHROMA_DB_PATH: str = "./chroma_db"
-    
-    model_config = SettingsConfigDict(env_file=".env")
-
-settings = Settings()
