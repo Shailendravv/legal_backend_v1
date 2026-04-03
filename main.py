832b20663420fc68ac65be56fb772e1010c68070
Shailendra2011 <shailendra.c@shyenatechyarns.in>
2026-04-03 07:41:24 +0530

    initial template of Project

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from backend!")
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI(title="Legal AI RAG Backend")
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
